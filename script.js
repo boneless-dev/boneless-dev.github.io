c61cc1b6598a22a5a96d6f68ecbf0a8ff262267a
boneless <mitosis2011152@gmail.com>
2022-05-15 13:30:20 -0400

    asd

---
+++ script.js
@@ -0,0 +1,16 @@
+function about_clicked() {
+    console.log("about");
+}
+
+function my_work_clicked() {
+    console.log("my work");
+}
+
+function portfolio_clicked() {
+    console.log("portfolio");
+}
+
+function paypal_shop_clicked() {
+    console.log("paypal shop");
+}
+
